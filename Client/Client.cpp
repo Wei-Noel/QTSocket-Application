f080c5e71f1bc5b4b14cfe0236001a1113dc551d
Lucas <73543663+Wei-Noel@users.noreply.github.com>
2023-07-01 20:29:23 +0800

    Add files via upload

---
+++ Client/Client.cpp
@@ -0,0 +1,82 @@
+﻿#include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QPlainTextEdit>
+#include <QTcpSocket>
+#include <QLineEdit>
+
+int main(int argc, char* argv[]) {
+	QApplication app(argc, argv);
+
+	QWidget window;
+	QVBoxLayout layout(&window);
+
+	QPlainTextEdit* output = new QPlainTextEdit(&window);
+	output->setReadOnly(true);
+	layout.addWidget(output);
+
+	QLineEdit* input = new QLineEdit(&window);
+	layout.addWidget(input);
+
+	QHBoxLayout* buttonLayout = new QHBoxLayout;
+	QPushButton* connectButton = new QPushButton("Connect to Server", &window);
+	buttonLayout->addWidget(connectButton);
+	QPushButton* sendButton = new QPushButton("Send", &window);
+	sendButton->setEnabled(false);
+	buttonLayout->addWidget(sendButton);
+	layout.addLayout(buttonLayout);
+
+	//创建一个QTcpSocket对象，用于和服务器通信
+	QTcpSocket clientSocket;
+	//当点击connectButton时，判断客户端是否已经连接到服务器，如果是，则断开连接，如果不是，则连接到服务器
+	QObject::connect(connectButton, &QPushButton::clicked, [&]() {
+		if (!clientSocket.isOpen()) {
+			// Connect to the server
+			clientSocket.connectToHost("localhost", 8888);
+		}
+		else
+		{
+			// Disconnect from the server
+			clientSocket.disconnectFromHost();
+			clientSocket.close();
+		}
+		});
+	//当客户端成功连接到服务器时，会触发connected信号
+	QObject::connect(&clientSocket, &QTcpSocket::connected, [&]() {
+		output->appendPlainText("Connected to server");
+		connectButton->setText("DisConnect");
+		sendButton->setEnabled(true);
+		});
+	//当客户端和服务器断开连接时，会触发disconnected信号
+	QObject::connect(&clientSocket, &QTcpSocket::disconnected, [&]() {
+		output->appendPlainText("Disconnected from server");
+		connectButton->setText("Connect");
+		sendButton->setEnabled(false);
+		clientSocket.close(); // Close the socket
+		});
+
+	//当客户端接收到服务器发送的数据时，会触发readyRead信号
+	QObject::connect(&clientSocket, &QTcpSocket::readyRead, [&]() {
+		while (clientSocket.canReadLine()) {
+			QByteArray data = clientSocket.readLine();
+			QString message = QString(data).trimmed();
+			output->appendPlainText("Server: " + message);
+		}
+		});
+
+	//当点击sendButton时，判断客户端是否已经连接到服务器，如果是，则发送数据到服务器
+	QObject::connect(sendButton, &QPushButton::clicked, [&]() {
+		QString message = input->text();
+		if (!message.isEmpty()) {
+			QByteArray data = message.toUtf8() + '\n';
+			output->appendPlainText("Client: " + message);
+			clientSocket.write(data);
+			input->clear();
+		}
+		});
+
+	window.show();
+
+	return app.exec();
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,106 @@
+﻿#include <QApplication>
+#include <QWidget>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QPlainTextEdit>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+
+int main(int argc, char* argv[]) {
+	QApplication app(argc, argv);
+
+	QWidget window;
+	QVBoxLayout layout(&window);
+
+	QPlainTextEdit* output = new QPlainTextEdit(&window);
+	output->setReadOnly(true);
+	layout.addWidget(output);
+
+	QLineEdit* input = new QLineEdit(&window);
+	layout.addWidget(input);
+
+	QHBoxLayout* buttonLayout = new QHBoxLayout;
+	QPushButton* startButton = new QPushButton("Start Server", &window);
+	buttonLayout->addWidget(startButton);
+	QPushButton* sendButton = new QPushButton("Send", &window);
+	buttonLayout->addWidget(sendButton);
+	layout.addLayout(buttonLayout);
+	// 创建一个QTcpServer对象，用于监听客户端的连接请求
+	QTcpServer server;
+	QList<QTcpSocket*> clientConnections;
+	// 当点击startButton时，判断服务器是否已经在监听，如果是，则停止监听，如果不是，则开始监听
+	QObject::connect(startButton, &QPushButton::clicked, [&]() {
+		if (server.isListening()) {
+			//如果服务正在运行
+			server.close();
+			for (auto connection : clientConnections) {
+				connection->close();
+				connection->deleteLater();
+			}
+			clientConnections.clear();
+			output->appendPlainText("Server stopped");
+			startButton->setText("Start Server");
+		}
+		else {
+			//服务没启动
+			if (server.listen(QHostAddress::Any, 8888)) {
+				output->appendPlainText("Server started. Listening on port 8888");
+				startButton->setText("Stop Server");
+			}
+			else {
+				output->appendPlainText("Failed to start server");
+			}
+		}
+		});
+
+
+	// 当有客户端连接到服务器时，会触发newConnection信号，我们在这里处理客户端的连接请求
+	QObject::connect(&server, &QTcpServer::newConnection, [&]() {
+		// 获取客户端的连接
+		QTcpSocket* clientSocket = server.nextPendingConnection();
+		// 将客户端的连接保存到clientConnections中
+		clientConnections.append(clientSocket);
+		output->appendPlainText("Client connected: " + clientSocket->peerAddress().toString());
+		// 当客户端有数据发送过来时，会触发readyRead信号，我们在这里处理客户端发送过来的数据
+		QObject::connect(clientSocket, &QTcpSocket::readyRead, [=]() {
+			// 读取客户端发送过来的数据
+			while (clientSocket->canReadLine()) {
+				QByteArray data = clientSocket->readLine();
+				QString message = QString(data).trimmed();// 去掉末尾的换行符
+				output->appendPlainText("Client: " + message);
+				// 将客户端发送过来的数据转发给所有的客户端
+				for (auto connection : clientConnections) {
+					if (connection != clientSocket) {
+						connection->write(data);
+					}
+				}
+			}
+			});
+		// 当客户端断开连接时，会触发disconnected信号，我们在这里处理客户端断开连接的事件
+		QObject::connect(clientSocket, &QTcpSocket::disconnected, [&]() {
+			if (clientConnections.contains(clientSocket)) {
+				clientConnections.removeOne(clientSocket);
+				clientSocket->deleteLater();
+				output->appendPlainText("Client disconnected");
+			}
+			});
+		});
+	// 当点击sendButton时，将input中的文本发送给所有的客户端
+	QObject::connect(sendButton, &QPushButton::clicked, [&]() {
+		QString message = input->text();
+		if (!message.isEmpty()) {
+			QByteArray data = message.toUtf8() + '\n';
+			output->appendPlainText("Server: " + message);
+			for (auto connection : clientConnections) {
+				connection->write(data);
+			}
+			input->clear();
+		}
+		});
+
+	window.show();
+
+	return app.exec();
+}
